51a2615ad1eebee4813bcb484e6eb564660b3948
echo.guan <echo.guan@perficient.com>
2017-08-03 14:35:27 +0800

    initial commit

---
+++ src/main/java/com/zxc/MyTM/MyTmApplication.java
@@ -0,0 +1,12 @@
+package com.zxc.MyTM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTmApplication.class, args);
+	}
+}
